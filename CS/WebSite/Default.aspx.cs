4531c5ad578321d72e54d572a13d93efb3868813
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Web.ASPxEditors;
+using DevExpress.Web.ASPxGridView;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void chbAll_Init(object sender, EventArgs e) {
+        ASPxCheckBox chb = sender as ASPxCheckBox;
+        GridViewDataItemTemplateContainer container = chb.NamingContainer as GridViewDataItemTemplateContainer;
+
+        chb.ClientInstanceName = String.Format("chbAll{0}", container.VisibleIndex);
+        chb.ClientSideEvents.CheckedChanged = String.Format("function (s, e) {{ OnAllCheckedChanged(s, e, {0}); }}", container.VisibleIndex);
+    }
+
+    protected void chb_Init(object sender, EventArgs e) {
+        ASPxCheckBox chb = sender as ASPxCheckBox;
+        GridViewDataItemTemplateContainer container = chb.NamingContainer as GridViewDataItemTemplateContainer;
+
+        chb.ClientInstanceName = String.Format("{0}{1}", chb.ID, container.VisibleIndex);
+        chb.ClientSideEvents.CheckedChanged = String.Format("function (s, e) {{ OnCheckedChanged(s, e, {0}); }}", container.VisibleIndex);
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to implement the "Select All" CheckBox editor in the ASPxGridView's row
+
+
+<p>This example shows how to implement the "Select All" CheckBox editor in the ASPxGridView's row.</p><p>If an end-user checks the ASPxCheckBox in the ASPxGridView's "All" column, other ASPxCheckBoxes in this row will set checked. If an end-user unchecks the ASPxCheckBox in the ASPxGridView's "All" column, other ASPxCheckBoxes in this row will set unchecked. If all CheckBoxes in the ASPxGridView's row are checked the ASPxCheckBox  in the ASPxGridView's "All" columns will set checked. If at least one of CheckBoxes in the ASPxGridView's row is unchecked, the ASPxCheckBox in the ASPxGridView's "All" columns will set unchecked.</p><p>To implement this scenario you can handle the <a href="ms-help://MS.MSDNQTR.v90.en/fxref_system.web.ui/html/658e894b-5faa-036a-e26c-a50f8eec5176.htm"><u>ASPxCheckBox.Init</u></a> and <a href="http://documentation.devexpress.com/#AspNet/DevExpressWebASPxEditorsScriptsASPxClientCheckBox_CheckedChangedtopic"><u>ASPxClientCheckBox.CheckedChanged</u></a> events for all CheckBoxs.</p>
+
+<br/>
+
+
